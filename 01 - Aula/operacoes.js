784c74b0d827c56c690f5cf346c771408ee82f3a
Akio Suzuki <kiosuzuki1@gmail.com>
2024-03-13 13:29:05 -0300

    exemplos-aula1

---
+++ 01 - Aula/operacoes.js
@@ -0,0 +1,23 @@
+function soma(x, y) {
+  return x + y;
+}
+  
+function subtracao(x, y) {
+  return x - y;
+}
+  
+const multiplicacao = function(x, y) {
+  return x * y;
+}
+
+const divisao = (x, y) => {
+  return x / y
+}
+
+module.exports = {
+  soma,
+  subtracao,
+  multiplicacao,
+  divisao
+};
+  
---
+++ 01 - Aula/solucoes.js
@@ -0,0 +1,9 @@
+const {soma, subtracao, multiplicacao, divisao} = require('./operacoes');
+
+console.log(soma(5,3))
+
+console.log(subtracao(10,7))
+
+console.log(multiplicacao(3,9))
+
+console.log(divisao(27,9))
