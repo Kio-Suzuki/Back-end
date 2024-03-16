3286f74be5b7e147b1f021386ed1427003764393
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2024-03-15 21:22:47 -0300

    Update ex04.js

--- 02 - Atividades Async/ex04.js
+++ 02 - Atividades Async/ex04.js
@@ -1,6 +1,10 @@
 const fs = require('fs').promises
 const path = require('path')
 
+let pasta = path.dirname(__dirname)
+
+console.log(pasta)
+
 async function nomesDiretorio(caminho) {
     await fs.
 }
