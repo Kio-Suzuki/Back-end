d0dbc7aa60b35ec319360b8c1520a4b5d382d9c1
Akio Suzuki <kiosuzuki1@gmail.com>
2024-04-01 09:51:35 -0300

    atividade

--- 03 - Atividade Express/01/index.js
+++ 03 - Atividade Express/01/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express() //criando servidor
 
 app.get('/main', (req, res) => {
-    res.send("<h1>Página com express</h1> <a href='http://localhost:3000/pag2'> Pagina 2 </a>")
+   res.sendFile(__dirname + '/html/index.html')
 })
 
-app.get('/pag2', (req, res) => {
-    res.send("<h1>Segunda página</h1> <a href='http://localhost:3000/main'> Pagina 2 </a>")
-})
+app.get('/public', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html');
+  });
 
 app.listen(3000, () => {
     console.log('funcionando...')
